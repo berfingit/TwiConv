f5ab3012e585ab916c433037a6461a39e62abd21
rafi <rafiderberliner@googlemail.com>
2021-01-14 16:46:36 +0100

    Create directory if not present. Handle zero-width space \u200B.

--- tokenize_tweets.py
+++ tokenize_tweets.py
@@ -9,8 +9,12 @@ tweets = "./tweets_fetched.jsonlines"
 author_directory = "./author"
 tweet_text_directory = "./text"
 
-# The withespace Tokenizer might not be sufficient, to handle the tokenizing. Not sure how it behaves with
-# the nospace unicode character.
+if not os.path.exists(author_directory):
+    os.makedirs(author_directory)
+
+if not os.path.exists(tweet_text_directory):
+    os.makedirs(tweet_text_directory)
+
 tk = WhitespaceTokenizer()
 
 
@@ -19,7 +23,9 @@ with open(tweets) as input_file:
         #tweet_id, autor_id, author_name, text
         json_tweet = json.loads(line)
         text_file_name = f"{json_tweet['tweet_id']}.txt"
-        tokenized_text = tk.tokenize(json_tweet["text"])
+        #Replace no space character as empty space before tokenizing.
+        processed_text = json_tweet["text"].replace("\u200B", " ")
+        tokenized_text = tk.tokenize(processed_text)
         author = json_tweet["author_name"]
         author_file = os.path.join(author_directory, text_file_name)
         text_file = os.path.join(tweet_text_directory, text_file_name)
